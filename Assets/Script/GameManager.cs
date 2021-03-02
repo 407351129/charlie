0cfbb91ffefa7126069b9eda86e41110beb7793c
Winnie1217 <w08031217@gmail.com>
2021-03-03 01:57:51 +0800

    無改

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,18 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class NewBehaviourScript : MonoBehaviour
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+ 
+public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public void OnStartGame(int SceneNumber)
     {
-        
+        SceneManager.LoadScene(SceneNumber);
     }
 }
