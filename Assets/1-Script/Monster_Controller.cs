21f41d05efcfc1e382a62ffb4a41c3ddfcdc4c46
Winnie1217 <w08031217@gmail.com>
2021-03-31 12:28:58 +0800

    2

---
+++ Assets/1-Script/Monster_Controller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster_Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Translate(Vector2.up);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * Time.deltaTime);
+    }
+}
