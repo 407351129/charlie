ff9913cfe37bf151ac4edf0c2ccff1ae605424a9
Winnie1217 <w08031217@gmail.com>
2021-04-04 01:27:57 +0800

    2

---
+++ Assets/1-Script/Fight/AttackEffect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackEffect : MonoBehaviour
+{
+    public float speed = 20f;
+    public int damage = 30;
+    public Rigidbody2D rb;
+    public GameObject impactEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        EnemyHealth enemy = hitInfo.GetComponent<EnemyHealth>();
+        if(enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+
+        Debug.Log(hitInfo.name);
+        Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/1-Script/Fight/DialogueText.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueText : MonoBehaviour
+{
+    public int lettersPerSecond;
+    public Text dialogueText;
+
+    public GameObject others;
+    public GameObject actions;
+    public GameObject turnAttack;
+    public GameObject turnBag;
+
+    public void SetDialogue(string dialogue)
+    {
+        dialogueText.text = dialogue;
+    }
+
+    public IEnumerator TypeDialogue(string dialogue)
+    {
+        dialogueText.text = "";
+        foreach(var letter in dialogue.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(1f/lettersPerSecond);
+        }
+    }
+
+    public void Enabledialogue(bool enabled)
+    {
+        dialogueText.enabled = enabled;
+    }
+
+    public void EnableOthers(bool enabled)
+    {
+        others.SetActive(enabled);
+    }
+
+    public void EnableActions(bool enabled)
+    {
+        actions.SetActive(enabled);
+    }
+
+    public void EnableAttack(bool enabled)
+    {
+        turnAttack.SetActive(enabled);
+    }
+
+    public void EnableBag(bool enabled)
+    {
+        turnBag.SetActive(enabled);
+    }
+}
