22859ff61462e8115d94484d521f14d6a38e4b2d
Joysomebody <407402588@gapp.fju.edu.tw>
2021-05-03 05:24:11 +0800

    sqlite files setting

--- Assets/1-Script/sqlitekit/questionData.cs
+++ Assets/1-Script/sqlitekit/questionData.cs
@@ -3,21 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class questionData : MonoBehaviour
 
 {
     public SqliteDatabase sqlDB;
-
     public Text show;
-
 
     // Use this for initialization
     void Start()
     {
         //打开测试数据库
         sqlDB = new SqliteDatabase("charlieSQLite.db");
-
 
         //以下insert , delete對資料庫內容沒有任何作用
 
@@ -28,40 +24,33 @@ public class questionData : MonoBehaviour
         PrintAllData();
 
         //根据id删除最后添加的数据 ,即使註解上面新增的數據 沒有作用
-
         sqlDB.ExecuteNonQuery("DELETE FROM Questions WHERE Questions_Id = (SELECT MAX(Questions_Id) FROM  Questions)");
         print("删除数据完毕");
 
         PrintAllData();
-        
+      
 
     }
 
     //控制台打印数据
     void PrintAllData()
-
     {
         //读取example表中所有数据
         DataTable dt = sqlDB.ExecuteQuery("SELECT * FROM Questions");
-
        
+
         //提取每行数据，遍历打印
         //int Questions_Id;
-
-       string Questions_Content;
+        string Questions_Content;
        string Questions_Symbol;
        string Questions_Chinese;
         int Questions_Reward;
         int Questions_Type;
 
 
-
-
         foreach (DataRow dr in dt.Rows)
         {
-
             //Questions_Id = (int)dr["Questions_Id"];
-
             Questions_Content = (string)dr["Questions_Content"];
             Questions_Symbol = (string)dr["Questions_Symbol"];
             Questions_Chinese = (string)dr["Questions_Chinese"];
@@ -76,25 +65,15 @@ public class questionData : MonoBehaviour
             print("name:" + Questions_Type);
 
 
-            //印在畫面的text上
-            show.text =   Questions_Content  +"\n"+ Questions_Symbol + "\n" + Questions_Chinese;
+            show.text = Questions_Content + "\n" + Questions_Symbol + "\n" + Questions_Chinese;
             //檢查在電腦上是否有連到資料庫用
             //Debug.Log("Questions_Content="+Questions_Content+"Questions_Symbol="+Questions_Symbol+"Questions_Chinese="+Questions_Chinese+"Questions_Reward="+Questions_Reward+"Questions_Type="+Questions_Type);
 
 
-            
-          
+
+
 
         }
-
-
-
-
-
     }
-
-
-
-        }
-
+}
 
