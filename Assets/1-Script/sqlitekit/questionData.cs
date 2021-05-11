2584776aa7aa8f8c9dad96e4d2bbf6f75e4afb9a
Joysomebody <407402588@gapp.fju.edu.tw>
2021-05-11 16:56:50 +0800

    資料庫語法

--- Assets/1-Script/sqlitekit/questionData.cs
+++ Assets/1-Script/sqlitekit/questionData.cs
@@ -11,6 +11,8 @@ public class questionData : MonoBehaviour
     public Text Chinese;
     public Text Pinin;
     public Text English;
+    //public int id;
+    //public int type;
 
 
     //提取每行数据，遍历打印
@@ -30,13 +32,15 @@ public class questionData : MonoBehaviour
         sqlDB = new SqliteDatabase("charlieSQLite.db");
 
         //读取example表中所有数据
-        DataTable dt = sqlDB.ExecuteQuery("SELECT * FROM Questions");
-
-      
+        //DataTable dt = sqlDB.ExecuteQuery("SELECT * FROM Questions");
 
 
+        DataTable dt = sqlDB.ExecuteQuery("SELECT * FROM Questions WHERE Questions_Id = 91");
 
-        foreach (DataRow dr in dt.Rows)
+
+
+
+        foreach (DataRow dr in dt.Rows)  //搜尋結果各欄儲存在dictionary 
         {
             Questions_Id = (int)dr["Questions_Id"];
             Questions_Content = (string)dr["Questions_Content"];
@@ -45,7 +49,7 @@ public class questionData : MonoBehaviour
             Questions_Reward = (int)dr["Questions_Reward"];
             Questions_Type = (int)dr["Questions_Type"];
 
-            //print("id:" + (int)dr["id"]);
+            //print("id:" + Questions_Id);
             //print("name:" + Questions_Content);
             //print("name:" + Questions_Symbol);
             //print("name:" + Questions_Chinese);
@@ -54,7 +58,9 @@ public class questionData : MonoBehaviour
 
 
 
-            English.text = Questions_Content;
+        }
+
+        English.text = Questions_Content;
             Pinin.text = Questions_Symbol;
             Chinese.text = Questions_Chinese;
 
@@ -66,21 +72,7 @@ public class questionData : MonoBehaviour
 
             Debug.Log("目前id:" + Questions_Id);
             Debug.Log("目前type:" + Questions_Type);
-
-        }
-
-
-
-
-
-        //添加数据 ,畫面可以顯示最後新增的資料
-        sqlDB
-            .ExecuteNonQuery("INSERT INTO Questions (Questions_Content, Questions_Symbol, Questions_Chinese, Questions_Reward, Questions_Type) VALUES('candy', 'tan','糖果',10,0)");
-        print("添加数据完毕");
-
-        PrintAllData();
-
-      
+ 
 
     }
 
@@ -88,7 +80,7 @@ public class questionData : MonoBehaviour
     void PrintAllData()
     {
         //读取example表中所有数据
-        DataTable dt = sqlDB.ExecuteQuery("SELECT * FROM Questions");
+        DataTable dt = sqlDB.ExecuteQuery("SELECT * FROM Questions" );
 
         //提取每行数据，遍历打印
         int Questions_Id;
@@ -132,31 +124,59 @@ public class questionData : MonoBehaviour
         }
 
 }
-        public void BattleQ(int question_id) //和怪物戰鬥時的題目
-        {
 
 
-
-        English.text = Questions_Content;
-        Pinin.text = Questions_Symbol;
-        Chinese.text = Questions_Chinese;
-
-
-
-        Debug.Log("目前英文:" + Questions_Content);
-        Debug.Log("目前讀音:" + Questions_Symbol);
-        Debug.Log("目前中文:" + Questions_Chinese);
-
-        Debug.Log("目前id:" + Questions_Id);
-        Debug.Log("目前type:" + Questions_Type);
-
-
-    }
-
-
-    public void OrderQ(int question_id) //訂單、送餐題目
+    public void ArriveQ(int question_type) //送餐至客戶家(短句) type 0
     {
 
+        DataTable dt = sqlDB.ExecuteQuery("SELECT * FROM Questions WHERE Questions_Type = 0 ORDER BY random() LIMIT 1"); //亂數選type 0 題目
+
+        foreach (DataRow dr in dt.Rows)  //搜尋結果各欄儲存在dictionary 
+        {
+            Questions_Id = (int)dr["Questions_Id"];
+            Questions_Content = (string)dr["Questions_Content"];
+            Questions_Symbol = (string)dr["Questions_Symbol"];
+            Questions_Chinese = (string)dr["Questions_Chinese"];
+            Questions_Reward = (int)dr["Questions_Reward"];
+            Questions_Type = (int)dr["Questions_Type"];
+
+        }
+
+        English.text = Questions_Content;
+        Pinin.text = Questions_Symbol;
+        Chinese.text = Questions_Chinese;
+
+
+
+        Debug.Log("目前英文:" + Questions_Content);
+        Debug.Log("目前讀音:" + Questions_Symbol);
+        Debug.Log("目前中文:" + Questions_Chinese);
+
+        Debug.Log("目前id:" + Questions_Id);
+        Debug.Log("目前type:" + Questions_Type);
+
+    }
+
+
+
+
+
+
+    public void OrderQ(int question_id) //訂單、送餐題目(食物單字) type 1
+    {
+
+        DataTable dt = sqlDB.ExecuteQuery("SELECT * FROM Questions WHERE Questions_Id = 1"); //選擇第一題
+
+        foreach (DataRow dr in dt.Rows)  //搜尋結果各欄儲存在dictionary 
+        {
+            Questions_Id = (int)dr["Questions_Id"];
+            Questions_Content = (string)dr["Questions_Content"];
+            Questions_Symbol = (string)dr["Questions_Symbol"];
+            Questions_Chinese = (string)dr["Questions_Chinese"];
+            Questions_Reward = (int)dr["Questions_Reward"];
+            Questions_Type = (int)dr["Questions_Type"];
+
+        }
 
 
         English.text = Questions_Content;
@@ -173,28 +193,86 @@ public class questionData : MonoBehaviour
         Debug.Log("目前type:" + Questions_Type);
 
     }
+
+
+
+    public void RunQ(int question_type) //逃跑題目(長句) type 2
+
+    {
+        DataTable dt = sqlDB.ExecuteQuery("SELECT * FROM Questions WHERE Questions_Type = 2 ORDER BY random() LIMIT 1"); ////亂數選type2句子
+
+        foreach (DataRow dr in dt.Rows)  //搜尋結果各欄儲存在dictionary 
+        {
+            Questions_Id = (int)dr["Questions_Id"];
+            Questions_Content = (string)dr["Questions_Content"];
+            Questions_Symbol = (string)dr["Questions_Symbol"];
+            Questions_Chinese = (string)dr["Questions_Chinese"];
+            Questions_Reward = (int)dr["Questions_Reward"];
+            Questions_Type = (int)dr["Questions_Type"];
+
+        }
+
+
+        English.text = Questions_Content;
+        Pinin.text = Questions_Symbol;
+        Chinese.text = Questions_Chinese;
+
+
+
+        Debug.Log("目前英文:" + Questions_Content);
+        Debug.Log("目前讀音:" + Questions_Symbol);
+        Debug.Log("目前中文:" + Questions_Chinese);
+
+        Debug.Log("目前id:" + Questions_Id);
+        Debug.Log("目前type:" + Questions_Type);
+
+    }
+
+
+
+
+
+    public void BattleQ(int question_id) //和怪物戰鬥時的題目(食物單字) type 3
+    {
+
+        DataTable dt = sqlDB.ExecuteQuery("SELECT * FROM Questions WHERE Questions_Id BETWEEN 1 AND 5 ORDER BY random() LIMIT 1"); //選擇第91題
+
+        foreach (DataRow dr in dt.Rows)  //搜尋結果各欄儲存在dictionary 
+        {
+            Questions_Id = (int)dr["Questions_Id"];
+            Questions_Content = (string)dr["Questions_Content"];
+            Questions_Symbol = (string)dr["Questions_Symbol"];
+            Questions_Chinese = (string)dr["Questions_Chinese"];
+            Questions_Reward = (int)dr["Questions_Reward"];
+            Questions_Type = (int)dr["Questions_Type"];
+
+        }
+
+
+        English.text = Questions_Content;
+        Pinin.text = Questions_Symbol;
+        Chinese.text = Questions_Chinese;
+
+
+
+        Debug.Log("目前英文:" + Questions_Content);
+        Debug.Log("目前讀音:" + Questions_Symbol);
+        Debug.Log("目前中文:" + Questions_Chinese);
+
+        Debug.Log("目前id:" + Questions_Id);
+        Debug.Log("目前type:" + Questions_Type);
+
+
+
+
     
 
-    public void RunQ(int question_type) //逃跑題目
-
-    {
-
-
-
-        English.text = Questions_Content;
-        Pinin.text = Questions_Symbol;
-        Chinese.text = Questions_Chinese;
-
-
-
-        Debug.Log("目前英文:" + Questions_Content);
-        Debug.Log("目前讀音:" + Questions_Symbol);
-        Debug.Log("目前中文:" + Questions_Chinese);
-
-        Debug.Log("目前id:" + Questions_Id);
-        Debug.Log("目前type:" + Questions_Type);
-
     }
+
+
+
+
+   
 
 
 
